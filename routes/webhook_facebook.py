e7aa041a44f13a6645bd9657fdf6abb17184324e
Cristian Salazar <CristianSalazarAtalaya@hotmail.com>
2020-01-26 18:07:55 -0500

    pruebas

--- routes/webhook_facebook.py
+++ routes/webhook_facebook.py
@@ -29,7 +29,7 @@ def facebook_challenge():
         print(output)
         #output = request.get_json()
         #recipient_id = output["entry"]["uid"]
-        dataentry = output["entry"]
+        dataentry = output["entry"][0]
         print(dataentry)
         print("*************************id*****")
         recipient_id=dataentry["messaging"][0]["sender"]["id"]
